4fe15c0afd6ccbc0948ee7ef2f02f44838236e14
Edgar Sanchez <edgar@Edgars-Air.attlocal.net>
2022-06-29 17:27:57 -0700

    Completed Exercise

--- cards.js
+++ cards.js
@@ -1,4 +1,29 @@
-// #1
+// #1 and #2
+// const deck = {
+//     async init() {
+//         let res = await axios.get('https://deckofcardsapi.com/api/deck/new/');
+//         this.deckId = res.data.deck_id
+//     },
+//     async shuffle() {
+//         let res = await axios.get(`https://deckofcardsapi.com/api/deck/${this.deckId}/shuffle`)
+//     },
+//     async drawCard() {
+//         let res = await axios.get(`https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=1`)
+//         for (card of res.data.cards) {
+//             console.log(`${card.value} of ${card.suit}.`)
+//         }
+//     }
+// }
+
+
+
+// #3
+const cardBtn = document.querySelector('#cardBtn');
+const deckBtn = document.querySelector('#createDeck');
+const shuffleBtn = document.querySelector('#shuffleDeck');
+const cardSpace = document.querySelector('#cardSpace');
+let cardImg;
+
 const deck = {
     async init() {
         let res = await axios.get('https://deckofcardsapi.com/api/deck/new/');
@@ -10,7 +35,24 @@ const deck = {
     async drawCard() {
         let res = await axios.get(`https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=1`)
         for (card of res.data.cards) {
+            cardImg = card.image
             console.log(`${card.value} of ${card.suit}.`)
         }
     }
 }
+
+deckBtn.addEventListener('click', function () {
+    deck.init();
+})
+shuffleBtn.addEventListener('click', function () {
+    deck.shuffle();
+})
+
+
+cardBtn.addEventListener('click', async function () {
+    cardSpace.innerHTML = "";
+    await deck.drawCard();
+    let card = document.createElement('img');
+    card.setAttribute('src', cardImg);
+    cardSpace.appendChild(card);
+})
